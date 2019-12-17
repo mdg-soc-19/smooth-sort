210936bef3ffeebb1add20de719339f145f542e3
Hemil Panchiwala <hpanchiwala@cs.iitr.ac.in>
2019-12-17 22:59:44 +0530

    Updated the example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,7 +24,6 @@ class BodyWidget extends StatefulWidget {
 }
 
 class BodyWidgetState extends State<BodyWidget> with TickerProviderStateMixin {
-
   SmoothSort smoothSort;
 
   @override
@@ -32,34 +31,280 @@ class BodyWidgetState extends State<BodyWidget> with TickerProviderStateMixin {
     super.initState();
 
     smoothSort = SmoothSort(
-        listType: 'list',
+        listType: 'grid',
         itemList: [
           Container(
-            color: Colors.red,
-            alignment: Alignment.center,
-            child: Text(
-              "A",
-              style: TextStyle(fontSize: 150.0),
+            color: Colors.white,
+            margin: EdgeInsets.fromLTRB(1.0, 1.0, 1.0, 1.0),
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/shirt1.jpeg',
+                  height: 140,
+                ),
+                Text("Peter England"),
+                Text(
+                  "Men Solid Formal Shirt",
+                  style: TextStyle(
+                    color: Colors.grey
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "        ₹499 ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0
+                      ),
+                    ),
+                    Text(
+                      "999 ",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        decoration: TextDecoration.lineThrough,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Text(
+                      "50% off",
+                      style: TextStyle(
+                        color: Colors.lightGreen,
+                        fontWeight: FontWeight.bold
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
           ),
           Container(
-            color: Colors.blueAccent,
-            alignment: Alignment.center,
-            child: Text(
-              "B",
-              style: TextStyle(fontSize: 150.0),
+            color: Colors.white,
+            margin: EdgeInsets.fromLTRB(1.0, 1.0, 1.0, 1.0),
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/shirt3.jpeg',
+                  height: 140,
+                ),
+                Text("Flying Machine"),
+                Text(
+                  "Men Solid Casual Spread Shirt",
+                  style: TextStyle(
+                      color: Colors.grey
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "        ₹694 ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0
+                      ),
+                    ),
+                    Text(
+                      "900 ",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          decoration: TextDecoration.lineThrough,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Text(
+                      "40% off",
+                      style: TextStyle(
+                          color: Colors.lightGreen,
+                          fontWeight: FontWeight.bold
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
           ),
           Container(
-            color: Colors.yellowAccent,
-            alignment: Alignment.center,
-            child: Text(
-              "C",
-              style: TextStyle(fontSize: 150.0),
+            color: Colors.white,
+            margin: EdgeInsets.fromLTRB(1.0, 1.0, 1.0, 1.0),
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/shirt4.jpeg',
+                  height: 140,
+                ),
+                Text("Raymond"),
+                Text(
+                  "Men Self Design Formal Shirt",
+                  style: TextStyle(
+                      color: Colors.grey
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "        ₹749 ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0
+                      ),
+                    ),
+                    Text(
+                      "2499 ",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          decoration: TextDecoration.lineThrough,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Text(
+                      "70% off",
+                      style: TextStyle(
+                          color: Colors.lightGreen,
+                          fontWeight: FontWeight.bold
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            margin: EdgeInsets.fromLTRB(1.0, 1.0, 1.0, 1.0),
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/shirt2.jpeg',
+                  height: 140,
+                ),
+                Text("Highlander"),
+                Text(
+                  "Men Casual Regular Shirt",
+                  style: TextStyle(
+                      color: Colors.grey
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "        ₹1019 ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0
+                      ),
+                    ),
+                    Text(
+                      "1699 ",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          decoration: TextDecoration.lineThrough,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Text(
+                      "40% off",
+                      style: TextStyle(
+                          color: Colors.lightGreen,
+                          fontWeight: FontWeight.bold
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            margin: EdgeInsets.fromLTRB(1.0, 1.0, 1.0, 1.0),
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/shirt6.jpeg',
+                  height: 140,
+                ),
+                Text("Van Heusen"),
+                Text(
+                  "Men Casual Shirt",
+                  style: TextStyle(
+                      color: Colors.grey
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "        ₹1429 ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0
+                      ),
+                    ),
+                    Text(
+                      "2599 ",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          decoration: TextDecoration.lineThrough,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Text(
+                      "45% off",
+                      style: TextStyle(
+                          color: Colors.lightGreen,
+                          fontWeight: FontWeight.bold
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            margin: EdgeInsets.fromLTRB(1.0, 1.0, 1.0, 1.0),
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/shirt5.jpeg',
+                  height: 140,
+                ),
+                Text("Levis"),
+                Text(
+                  "Men Casual Cut Away Shirt",
+                  style: TextStyle(
+                      color: Colors.grey
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "        ₹1499 ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0
+                      ),
+                    ),
+                    Text(
+                      "2999 ",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          decoration: TextDecoration.lineThrough,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Text(
+                      "50% off",
+                      style: TextStyle(
+                          color: Colors.lightGreen,
+                          fontWeight: FontWeight.bold
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
           ),
         ],
-        itemIdList: [1, 2, 0],
+        itemIdList: [4, 5, 3, 2, 0, 1],
         animationType: 'scale');
   }
 
@@ -71,7 +316,7 @@ class BodyWidgetState extends State<BodyWidget> with TickerProviderStateMixin {
         children: <Widget>[
           smoothSort,
           RaisedButton(
-            child: Text("Sort"),
+            child: Text("Sort By Price"),
             onPressed: () {
               smoothSort.onPress();
             },
